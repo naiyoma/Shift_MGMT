53a7e2db9434d38c197bbbb14f6f6d443422008d
naiyoma <lankas.aurelia@gmail.com>
2023-01-17 22:00:17 +0300

    user registration

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './../styles.css';
 function Header() {
     return(
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+        <h1 className="heading">Hello World</h1>
     )
 }
 export default Header;
